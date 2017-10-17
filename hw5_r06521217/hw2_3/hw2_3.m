348c854fda3f6cc4c44e9bed79643442ef4c6cba
skyran1278 <skyran1278@gmail.com>
2017-10-17 22:44:33 +0800

    整合 callback

--- hw5_r06521217/hw2_3/hw2_3.m
+++ hw5_r06521217/hw2_3/hw2_3.m
@@ -3,29 +3,29 @@
 % 消除前一次作業
 clc; clear; close all;
 
-% for loop
+% for loop method
 
 tic;
-printProbability(100);
-printProbability(10000);
-printProbability(1000000);
+printProbability(100, @bigTest);
+printProbability(10000, @bigTest);
+printProbability(1000000, @bigTest);
 forLoopTime = toc;
 
 
-% matrix
+% matrix method
 fprintf('\n\n-------------------\nmatrix method contrast:\n\n');
 
 tic;
-printProbabilityForSqrtTimes(100);
-printProbabilityForSqrtTimes(10000);
-printProbabilityForSqrtTimes(1000000);
+printProbability(100, @bigTestByMatrixMethod);
+printProbability(10000, @bigTestByMatrixMethod);
+printProbability(1000000, @bigTestByMatrixMethod);
 matrixTime = toc;
 
 fprintf('\nmatrix method is faster than for loop method: %f times\n', forLoopTime / matrixTime);
 
 
-function f = printProbability(testTimes)
-  fprintf('after %d tests, probability of failure is %f \n', testTimes, bigTest(testTimes));
+function f = printProbability(testTimes, callback)
+  fprintf('after %d tests, probability of failure is %f \n', testTimes, callback(testTimes));
 end
 
 
@@ -58,12 +58,7 @@ function probability = bigTest(testTimes)
 end
 
 
-function f = printProbabilityForSqrtTimes(testTimes)
-  fprintf('after %.0f tests, probability of failure is %f \n', testTimes, bigTestForSqrtTimes(testTimes));
-end
-
-
-function probability = bigTestForSqrtTimes(testTimes)
+function probability = bigTestByMatrixMethod(testTimes)
 %bigTest - probability of failure
 %
 % Syntax: probability = bigTest(testTimes)
@@ -71,9 +66,9 @@ function probability = bigTestForSqrtTimes(testTimes)
 % Long probability of failure
 
   L = 5;
-  E = 7.7 * 10 ^ 6 + ( 0.1 * 10 ^ 6 * rand(sqrt(testTimes)) );
-  I = 8 * 10 ^ (-4) + ( 2 * 10 ^ (-4) * rand(sqrt(testTimes)) );
-  w = 10 + 15 * rand(sqrt(testTimes));
+  E = 7.7 * 10 ^ 6 + ( 0.1 * 10 ^ 6 * rand(1, testTimes) );
+  I = 8 * 10 ^ (-4) + ( 2 * 10 ^ (-4) * rand(1, testTimes) );
+  w = 10 + 15 * rand(1, testTimes);
 
   exceedLimitTimes = sum(sum(0.0069 .* w .* L .^ 4 ./ E ./ I > L / 360));
 

22ed9a43d8d50085eee37d376ed460b4334b19e4
乃宥然 <skyran@naiyourande-MacBook-Pro.local>
2017-10-28 14:01:34 +0800

    完成作業

--- programming 1/FRAME17.m
+++ programming 1/FRAME17.m
@@ -16,7 +16,7 @@
 %		   create an input file and run the program to write out
 %          the data (in subroutine INPUT) to see if the output
 %          data are the same as the input data. In addition, use function
-%          drawingStructure to check the geometry of the structures.
+%          DrawingStructure to check the geometry of the structures.
 %      (4) Sumbit the following to CEIBA in archive file (*.zip or *.rar):
 %          (a) Program source code "FRAME17.m"
 %          (b) Input file "*.ipt"
@@ -59,127 +59,136 @@ function FRAME17(FILENAME)
 %   SPACE   TRUSS   5    3    3
 %   SPACE   FRAME   6    3    6
 
-FTYPE = {'BEAM'; 'PLANE TRUSS'; 'PLANE FRAME'; 'PLANE GRID'; ...
-    'SPACE TRUSS'; 'SPACE FRAME'};
-IPR = [1, 2, 2, 2, 3, 3; 2, 2, 3, 3, 3, 6];
+    FTYPE = {'BEAM'; 'PLANE TRUSS'; 'PLANE FRAME'; 'PLANE GRID'; ...
+        'SPACE TRUSS'; 'SPACE FRAME'};
+    IPR = [1, 2, 2, 2, 3, 3; 2, 2, 3, 3, 3, 6];
 
-if nargin == 0 % no input argument
-    % Open file with user interface
-    [~, FILENAME] = fileparts(uigetfile('*.ipt', 'Select Input file'));
-end
-
-% Get starting time
-startTime = clock;
-
-% FILENAME.ipt is the input data file.
-% FILENAME.dat includes the output of the input data and
-%   the nodal displacements and member end forces.
-
-IREAD = fopen([FILENAME '.ipt'], 'r');
-
-% Read in the problem title and the structural data
-TITLE = fgets(IREAD);
-FUNIT = fgets(IREAD);
-LUNIT = fgets(IREAD);
-
-ID = '*';
-HEADLINE(ID,IREAD);
-
-line = fgets(IREAD);
-args = str2num(line); % 可回傳 matrix
-[NNOD, NBC, NMAT, NSEC, ITP, NNE, IFORCE] = deal(args(1), args(2), args(3), args(4), args(5), args(6), args(7));
-NCO = IPR(1, ITP);
-NDN = IPR(2, ITP);
-NDE = NDN * NNE;
-
-% Read the remaining data
-% [COOR,~] = INPUT(IREAD,ID,NNOD,NCO,~);
-[COOR, NFIX, EXLD, IDBC, VECTY, FEF, PROP, SECT] = ...
-INPUT(IREAD, ID, NNOD, NBC, NMAT, NSEC, ITP, NCO, NDN, NDE, IFORCE);
-
-fclose(IREAD);
-
-%DrawingStructure
-% FORMAT='-';
-% drawingStructure(ITP, COOR, IDBC, NBC, LUNIT, FORMAT);
-
-% write out the data
-IWRITE=fopen(FILENAME,'w');
-
-fprintf(IWRITE, '%s', TITLE);
-fprintf(IWRITE, '%s', FUNIT);
-fprintf(IWRITE, '%s', LUNIT);
-fprintf(IWRITE, '\n');
-
-fprintf(IWRITE, '* NNOD NBC NMAT NSEC ITP NNE IFORCE');
-fprintf(IWRITE, '\n');
-fprintf(IWRITE, '%d %d %d %d %d %d %d', NNOD, NBC, NMAT, NSEC, ITP, NNE, IFORCE);
-fprintf(IWRITE, '\n');
-fprintf(IWRITE, '\n');
-
-fprintf(IWRITE, '* COOR (m)');
-fprintf(IWRITE, '\n');
-for col = 1 : NNOD
-    fprintf(IWRITE, '%d', col);
-    for row = 1 : NCO
-        fprintf(IWRITE, ' ');
-        fprintf(IWRITE, '%f', COOR(row, col));
+    if nargin == 0 % no input argument
+        % Open file with user interface
+        [~, FILENAME] = fileparts(uigetfile('*.ipt', 'Select Input file'));
     end
-    fprintf(IWRITE, '\n');
+
+    % Get starting time
+    startTime = clock;
+
+    % FILENAME.ipt is the input data file.
+    % FILENAME.dat includes the output of the input data and
+    %   the nodal displacements and member end forces.
+
+    IREAD = fopen([FILENAME '.ipt'], 'r');
+
+    % Read in the problem title and the structural data
+    TITLE = fgets(IREAD);
+    FUNIT = fgets(IREAD);
+    LUNIT = fgets(IREAD);
+
+    ID = '*';
+    HeadLine(ID,IREAD);
+
+    line = fgets(IREAD);
+    args = str2num(line); % 可回傳 matrix
+    [NNOD, NBC, NMAT, NSEC, ITP, NNE, IFORCE] = deal(args(1), args(2), args(3), args(4), args(5), args(6), args(7));
+    NCO = IPR(1, ITP);
+    NDN = IPR(2, ITP);
+    NDE = NDN * NNE;
+
+    % Read the remaining data
+    % [COOR,~] = INPUT(IREAD,ID,NNOD,NCO,~);
+    [COOR, NFIX, EXLD, IDBC, VECTY, FEF, PROP, SECT] = ...
+    INPUT(FILENAME, TITLE, FUNIT, LUNIT, IREAD, ID, NNOD, NBC, NMAT, NSEC, ITP, NCO, NDN, NDE, IFORCE, NNE);
+
+    fclose(IREAD);
+
+    % DrawingStructure
+    FORMAT='-';
+    DrawingStructure(ITP, COOR, IDBC, NBC, LUNIT, FORMAT);
+
+
+    % ^^* UP TO HERE  --- PROG 1 ^^*
+
+    %
+    % % DOF numbering
+    % [IDND,NEQ] = IDMAT( ~ );
+    %
+    % % Compute the member DOF table:  LM(NDE,NBC)
+    % LM = MEMDOF( ~ );
+    %
+    % % Compute the semi-band width,NSBAND, of the global stiffness matrix
+    % NSBAND = SEMIBAND( ~ );
+    %
+    % %Form the global load vector GLOAD(NEQ) from the concentrated nodal loads
+    % GLOAD = LOAD(EXLD, ~ );
+    %
+    % % ^^* UP TO HERE  --- PROG 2 ^^*
+    %
+    % % Form the global stiffness matrix GLK(NEQ,NSBAND) and obtain the
+    % % equivalent nodal vector by assembling -(fixed-end forces) of each member
+    % % into the load vector.
+    % [GLK,GLOAD] = FORMKP(COOR,IDBC,VECTY,PROP,SECT,LM,FEF,GLOAD,NNOD,NBC,NMAT...
+    %     ,NSEC,IFORCE,ITP,NCO,NDN,NDE,NNE,NEQ);
+    %
+    % % ^^* UP TO HERE  --- PROG 3 ^^*
+    %
+    % DISP = SOLVE(GLK, ~ );
+    %
+    % % Determine the member end forces ELFOR(NDE,NBC)
+    % ELFOR = FORCE( ~ );
+    %
+    % % Get ending time and count the elapased time
+    % endTime = clock;
+    %
+    % % Print out the results
+    % IWRITE = fopen([FILENAME '.dat'], 'w');
+    % OUTPUT( ~ );
+    % fclose(IWRITE);
+    %
+    % IGW = fopen([FILENAME '.txt'], 'w');
+    % GRAPHOUTPUT(IGW,COOR,NFIX,EXLD,IDBC,FEF,PROP,SECT,LM,IDND,DISP,ELFOR,NNOD,...
+    %     NDN,NCO,NDE,NEQ,NBC,NMAT,NSEC,ITP,NNE,IFORCE,FUNIT,LUNIT);
+    % fclose(IGW);
+    %
+    % % ^^* UP TO HERE  --- PROG 4 ^^*
+
 end
 
-% function f = fprintfWithIWRITE(input)
-%     f =
-% end
+function [] = WriteOutMatrix(FILEID, TITLE, MATRIX, FORMAT)
+% 寫入 TITLE 與 MATRIX 到 FILEID
 
-% ^^* UP TO HERE  --- PROG 1 ^^*
+    [rowLength, colLength] = size(MATRIX);
 
-%
-% % DOF numbering
-% [IDND,NEQ] = IDMAT( ~ );
-%
-% % Compute the member DOF table:  LM(NDE,NBC)
-% LM = MEMDOF( ~ );
-%
-% % Compute the semi-band width,NSBAND, of the global stiffness matrix
-% NSBAND = SEMIBAND( ~ );
-%
-% %Form the global load vector GLOAD(NEQ) from the concentrated nodal loads
-% GLOAD = LOAD(EXLD, ~ );
-%
-% % ^^* UP TO HERE  --- PROG 2 ^^*
-%
-% % Form the global stiffness matrix GLK(NEQ,NSBAND) and obtain the
-% % equivalent nodal vector by assembling -(fixed-end forces) of each member
-% % into the load vector.
-% [GLK,GLOAD] = FORMKP(COOR,IDBC,VECTY,PROP,SECT,LM,FEF,GLOAD,NNOD,NBC,NMAT...
-%     ,NSEC,IFORCE,ITP,NCO,NDN,NDE,NNE,NEQ);
-%
-% % ^^* UP TO HERE  --- PROG 3 ^^*
-%
-% DISP = SOLVE(GLK, ~ );
-%
-% % Determine the member end forces ELFOR(NDE,NBC)
-% ELFOR = FORCE( ~ );
-%
-% % Get ending time and count the elapased time
-% endTime = clock;
-%
-% % Print out the results
-% IWRITE = fopen([FILENAME '.dat'], 'w');
-% OUTPUT( ~ );
-% fclose(IWRITE);
-%
-% IGW = fopen([FILENAME '.txt'], 'w');
-% GRAPHOUTPUT(IGW,COOR,NFIX,EXLD,IDBC,FEF,PROP,SECT,LM,IDND,DISP,ELFOR,NNOD,...
-%     NDN,NCO,NDE,NEQ,NBC,NMAT,NSEC,ITP,NNE,IFORCE,FUNIT,LUNIT);
-% fclose(IGW);
-%
-% % ^^* UP TO HERE  --- PROG 4 ^^*
+    % 比較簡單直覺的寫法
+    % for col = 1 : colLength
+
+    %     % 計數器
+    %     fprintf(FILEID, '%d', col);
+
+    %     for row = 1 : rowLength
+    %         fprintf(FILEID, ' ');
+    %         fprintf(FILEID, '%f', MATRIX(row, col));
+    %     end
+
+    %     fprintf(FILEID, '\n');
+    % end
+
+    index = 1 : colLength;
+
+    indexMatrix = [index; MATRIX];
+
+    fprintf(FILEID, TITLE);
+    fprintf(FILEID, '\n');
+
+    % fprintf 一列一列印
+    % 這個方法要熟悉 matlab 的 function 才比較直覺
+    fprintf(FILEID, ['%d\t', repmat(FORMAT, 1, rowLength), '\n'], indexMatrix);
+
+    fprintf(FILEID, '\n');
 
 end
 
-function HEADLINE(ID,IREAD)
+
+function HeadLine(ID,IREAD)
+% 這是用來忽略空白列與註解列
+
     while ~feof(IREAD)
         temp = fgets(IREAD);
         if ~isempty(temp) && temp(1) == ID
@@ -188,7 +197,7 @@ function HEADLINE(ID,IREAD)
     end
 end
 
-function [COOR, NFIX, EXLD, IDBC, VECTY, FEF, PROP, SECT] = INPUT(IREAD, ID, NNOD, NBC, NMAT, NSEC, ITP, NCO, NDN, NDE, IFORCE)
+function [COOR, NFIX, EXLD, IDBC, VECTY, FEF, PROP, SECT] = INPUT(FILENAME, TITLE, FUNIT, LUNIT, IREAD, ID, NNOD, NBC, NMAT, NSEC, ITP, NCO, NDN, NDE, IFORCE, NNE)
 %..........................................................................
 %
 %    PURPOSE: Reads in the following data: nodal coordinates,
@@ -247,45 +256,86 @@ function [COOR, NFIX, EXLD, IDBC, VECTY, FEF, PROP, SECT] = INPUT(IREAD, ID, NNO
 %                       (5,*) = omitted.
 %..........................................................................
 
-% COOR - Nodal coordinates
-HEADLINE(ID, IREAD);
-COOR = ReadMatrix(IREAD, NCO, NNOD);
+    % COOR - Nodal coordinates
+    HeadLine(ID, IREAD);
+    COOR = ReadMatrix(IREAD, NCO, NNOD);
 
-% NFIX 束制條件
-HEADLINE(ID, IREAD);
-NFIX = ReadMatrix(IREAD, NDN, NNOD);
+    % NFIX 束制條件
+    HeadLine(ID, IREAD);
+    NFIX = ReadMatrix(IREAD, NDN, NNOD);
 
-% External Load 外力
-HEADLINE(ID, IREAD);
-EXLD = ReadMatrix(IREAD, NDN, NNOD);
+    % External Load 外力
+    HeadLine(ID, IREAD);
+    EXLD = ReadMatrix(IREAD, NDN, NNOD);
 
-% IDBC 幾何形狀
-HEADLINE(ID, IREAD);
-IDBC = ReadMatrix(IREAD, 5, NBC);
+    % IDBC 幾何形狀
+    HeadLine(ID, IREAD);
+    IDBC = ReadMatrix(IREAD, 5, NBC);
 
-% VECTY
-if ITP == 6
-    HEADLINE(ID, IREAD);
-    VECTY = ReadMatrix(IREAD, 3, NBC);
-else
-    VECTY = [];
-end
+    % VECTY
+    if ITP == 6
+        HeadLine(ID, IREAD);
+        VECTY = ReadMatrix(IREAD, 3, NBC);
+    else
+        VECTY = [];
+    end
 
-% IFORCE
-if IFORCE == 2
-    HEADLINE(ID, IREAD);
-    FEF = ReadMatrix(IREAD, NDE, NBC);
-else
-    FEF = [];
-end
+    % IFORCE
+    if IFORCE == 2
+        HeadLine(ID, IREAD);
+        FEF = ReadMatrix(IREAD, NDE, NBC);
+    else
+        FEF = [];
+    end
 
-% PROP 材料性質
-HEADLINE(ID, IREAD);
-PROP = ReadMatrix(IREAD, 5, NMAT);
+    % PROP 材料性質
+    HeadLine(ID, IREAD);
+    PROP = ReadMatrix(IREAD, 5, NMAT);
 
-% SECT 斷面大小
-HEADLINE(ID, IREAD);
-SECT = ReadMatrix(IREAD, 5, NSEC);
+    % SECT 斷面大小
+    HeadLine(ID, IREAD);
+    SECT = ReadMatrix(IREAD, 5, NSEC);
+
+    %..........................................................................
+    % write out the input data to check
+
+    IWRITE=fopen([FILENAME '.opt'],'w');
+
+    fprintf(IWRITE, '%s', TITLE);
+    fprintf(IWRITE, '%s', FUNIT);
+    fprintf(IWRITE, '%s', LUNIT);
+    fprintf(IWRITE, '\n');
+
+    fprintf(IWRITE, '* NNOD NBC NMAT NSEC ITP NNE IFORCE');
+    fprintf(IWRITE, '\n');
+    fprintf(IWRITE, '%d %d %d %d %d %d %d', NNOD, NBC, NMAT, NSEC, ITP, NNE, IFORCE);
+    fprintf(IWRITE, '\n');
+    fprintf(IWRITE, '\n');
+
+    WriteOutMatrix(IWRITE, '* COOR (m)', COOR, '%.3f\t');
+
+    WriteOutMatrix(IWRITE, '* NFIX', NFIX, '%d\t');
+
+    WriteOutMatrix(IWRITE, '* External Load (kN)', EXLD, '%.3f\t');
+
+    WriteOutMatrix(IWRITE, '* IDBC', IDBC, '%d\t');
+
+    % VECTY
+    if ITP == 6
+        WriteOutMatrix(IWRITE, '* VECTY', VECTY, '%.3f\t');
+    end
+
+    % IFORCE
+    if IFORCE == 2
+        WriteOutMatrix(IWRITE, '* VECTY', FEF, '%.3f\t');
+    end
+
+
+    WriteOutMatrix(IWRITE, '* PROP', PROP, '%.3f\t');
+
+    WriteOutMatrix(IWRITE, '* SECT', SECT, '%.3f\t');
+
+    %..........................................................................
 
 
 end
@@ -306,58 +356,60 @@ function mat = ReadMatrix(IREAD, row, col)
 end
 
 
-function drawingStructure(ITP, COOR, IDBC, NBC, LUNIT, FORMAT)
-switch ITP
-    case 1
-        for e=1:NBC
-            plot([COOR(IDBC(1,e)),COOR(IDBC(2,e))],[0,0],FORMAT,'linewidth',2)
-            xlabel(['X ', LUNIT])
-            title('Beam')
-            hold on
-        end
-        hold off
-    case {2,3}
-        for e=1:NBC
-            plot([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
-                [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],FORMAT,'linewidth',2)
-            xlabel(['X ', LUNIT])
-            ylabel(['Y ', LUNIT])
-            if ITP==2
-                title('2D truss')
-            else
-                title('2D frame')
+function DrawingStructure(ITP, COOR, IDBC, NBC, LUNIT, FORMAT)
+% 沒有看裡面的內容
+
+    switch ITP
+        case 1
+            for e=1:NBC
+                plot([COOR(IDBC(1,e)),COOR(IDBC(2,e))],[0,0],FORMAT,'linewidth',2)
+                xlabel(['X ', LUNIT])
+                title('Beam')
+                hold on
             end
-            hold on
-        end
-        axis equal
-        hold off
-    case 4
-        for e=1:NBC
-            plot([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
-                [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],FORMAT,'linewidth',2)
-            xlabel(['X ', LUNIT])
-            ylabel(['Z ', LUNIT])
-            title('grid')
-            hold on
-        end
-        axis equal
-        hold off
-    case {5,6}
-        for e=1:NBC
-            plot3([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
-                [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],...
-                [COOR(3,IDBC(1,e)),COOR(3,IDBC(2,e))],FORMAT,'linewidth',2)
-            xlabel(['X ', LUNIT])
-            ylabel(['Y ', LUNIT])
-            zlabel(['Z ', LUNIT])
-            if ITP==5
-                title('3D truss')
-            else
-                title('3D frame')
+            hold off
+        case {2,3}
+            for e=1:NBC
+                plot([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
+                    [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],FORMAT,'linewidth',2)
+                xlabel(['X ', LUNIT])
+                ylabel(['Y ', LUNIT])
+                if ITP==2
+                    title('2D truss')
+                else
+                    title('2D frame')
+                end
+                hold on
             end
-            hold on
-        end
-        axis equal
-        hold off
-end
+            axis equal
+            hold off
+        case 4
+            for e=1:NBC
+                plot([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
+                    [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],FORMAT,'linewidth',2)
+                xlabel(['X ', LUNIT])
+                ylabel(['Z ', LUNIT])
+                title('grid')
+                hold on
+            end
+            axis equal
+            hold off
+        case {5,6}
+            for e=1:NBC
+                plot3([COOR(1,IDBC(1,e)),COOR(1,IDBC(2,e))],...
+                    [COOR(2,IDBC(1,e)),COOR(2,IDBC(2,e))],...
+                    [COOR(3,IDBC(1,e)),COOR(3,IDBC(2,e))],FORMAT,'linewidth',2)
+                xlabel(['X ', LUNIT])
+                ylabel(['Y ', LUNIT])
+                zlabel(['Z ', LUNIT])
+                if ITP==5
+                    title('3D truss')
+                else
+                    title('3D frame')
+                end
+                hold on
+            end
+            axis equal
+            hold off
+    end
 end

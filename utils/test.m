0ef87c43b0e6197f4f1ef51803502d406a9fd062
skyran1278 <skyran1278@gmail.com>
2018-01-06 15:00:13 +0800

    測試

--- utils/test.m
+++ utils/test.m
@@ -2,10 +2,29 @@
 clc; clear; close all;
 
 % 需要準確區分矩陣乘法 .* 逐元素 * 一般矩陣乘法
-syms E A;
+syms a b c d e f g h k l m n o p q r s t u v w x y z;
 
 
-2000 * [ 10 1 0];
-2000 .* [ 10 1 0];
+D = [
+  a b 0 0;
+  c d 0 0;
+  0 0 e f;
+  0 0 g h;
+];
 
-k = E * A * [ 1 2 3]
+D1 = D(1 : 2, 1 : 2);
+D2 = D(3 : 4, 3 : 4);
+
+C = [
+  k l m n;
+  o p q r;
+];
+
+C1 = C(:, 1 : 2);
+C2 = C(:, 3 : 4);
+
+Dr = C * D * C.';
+
+DrPlus = C1 * D1 * C1.' + C2 * D2 * C2.'
+
+Dr == DrPlus

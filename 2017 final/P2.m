0e66d3d5cc07fa9404e5c1d901df5fc979d7fc77
skyran1278 <skyran1278@gmail.com>
2018-01-11 01:00:51 +0800

    完成第三題中

--- 2017 final/P2.m
+++ 2017 final/P2.m
@@ -126,23 +126,7 @@ OUTPUT(IWRITE, TITLE, FILENAME, FTYPE, FUNIT, LUNIT, startTime, endTime, ...
     NNOD, NBC, NMAT, NSEC, NEQ, NCO, NDN, NNE, ITP, COOR, NFIX, PROP, SECT, IDBC, IDND, ...
     VECTY, EXLD, IFORCE, FEF, DELTA, ELFOR, NSBAND);
 
-V = 0;
-M = 0;
-C = 0;
-
-for  IB = 1 : NBC
-    [~, RL] = ROTATION(COOR, VECTY, IDBC, IB, ITP, NCO, NDE);
-    A = SECT(1, IDBC(4, IB));
-    RHO = PROP(4, IDBC(3, IB));
-    COST = PROP(3, IDBC(3, IB));
-    V = V + A * RL;
-    M = M + RHO * A * RL;
-    C = C + COST * RHO * A * RL;
-end
-
-fprintf(IWRITE, 'Total Volume :%f m^3\r\n', V);
-fprintf(IWRITE, 'Total Mass :%f kg\r\n', M);
-fprintf(IWRITE, 'Total Cost :%f NTD\r\n', C);
+CalVolumeMassCost(IWRITE, NBC, COOR, VECTY, IDBC, ITP, NCO, NDE, SECT, PROP);
 
 fclose(IWRITE);
 
